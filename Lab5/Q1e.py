6fb2890dca45cd3af0a5a201ad33f8deffbd12ec
Ujwal Ramachandran <60609567+Ujwal-Ramachandran@users.noreply.github.com>
2021-05-08 14:26:14 +0530

    Delete Lab5 directory

--- Lab5/Q1e.py
+++
@@ -1,8 +0,0 @@
-def recSum(n):
-    if(n==0):
-        return 0
-    else:
-        return n + recSum(n-1)
-       
-n = int(input("Enter a number : "))
-print('Sum of first',n,'numbers is =',recSum(n))
--- Lab5/Q1g.py
+++
@@ -1,8 +0,0 @@
-ex = 0
-while ex!=1:
-    no = int(input('Enter a number :  '))
-    print('Binary of',no,'is = ',bin(no).replace('0b',''))
-    print('Octal of',no,'is = ',oct(no).replace('0o',''))
-    print('Hexadecimal of',no,'is = ',hex(no).replace('0x',''))
-    print()
-    ex = int(input('Press 1 to exit. To try again press anything else :  '))
